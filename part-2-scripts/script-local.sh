215421bff59aa1a6ffa620c665e697f4018c096b
MohtSing12 <mohitdelhi0987@gmail.com>
2025-06-08 12:33:13 +0530

    ec2 url change

--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "todo-app-class.pem" ubuntu@ec2-13-218-238-92.compute-1.amazonaws.com  "sudo bash ~/deploy.sh"
+ssh -t -i "todo-app-class.pem" ubuntu@ec2-3-92-77-21.compute-1.amazonaws.com  "sudo bash ~/deploy.sh"
