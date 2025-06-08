3c35cb140399c63377df86f1613c3bab9f1a8ce7
MohtSing12 <mohitdelhi0987@gmail.com>
2025-06-08 12:53:36 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
-app.get("/",(req,res)=>res.json({msg:"Hello this is to check ci pipeline"}))
+app.get("/",(req,res)=>res.json({msg:"To check ci pipeline!!"}))
 
 
 // Connnect to MongoDB
