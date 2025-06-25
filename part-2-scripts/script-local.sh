70ef0005504fbcc86bf289d686da1ca73b054cfe
MohtSing12 <mohitdelhi0987@gmail.com>
2025-06-26 00:00:53 +0530

    new ip

--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "todo-app-class.pem" ubuntu@ec2-3-92-77-21.compute-1.amazonaws.com  "sudo bash ~/deploy.sh"
+ssh -t -i "todo-app-class.pem" ubuntu@ec2-44-206-240-82.compute-1.amazonaws.com  "sudo bash ~/deploy.sh"
