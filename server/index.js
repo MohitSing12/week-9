4b6de169d2e729f5568b8cbf262fb5ca670a14bf
MohtSing12 <mohitdelhi0987@gmail.com>
2025-06-26 00:44:36 +0530

    message

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
-app.get("/",(req,res)=>res.json({msg:"To check ci pipeline!!"}))
+app.get("/",(req,res)=>res.json({msg:"To check ci pipeline two times!!"}))
 
 
 // Connnect to MongoDB
